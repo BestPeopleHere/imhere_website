83ae2dfb99dd26ea3e46a3dbf8256b099a76dce2
saranin-programmer <aldan2550@gmail.com>
2024-11-07 15:39:16 +0700

    починил

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    {
+      name: 'auto-import-css',
+      transform(code, id) {
+        if (id.endsWith('.tsx')) {
+          const cssPath = id.replace(/\.tsx$/, '.css');
+
+          //Пример асинхронной проверки существования файла
+          return new Promise((resolve) => {
+
+            fs.access(cssPath, fs.constants.F_OK, (err) => {
+              if (!err) {
+                resolve(`import '${cssPath}';\n` + code);
+              } else {
+                console.warn(`CSS file not found: ${cssPath}`);
+                resolve(code); // Продолжение работы даже если файла нет
+              }
+            });
+          });
+        }
+        return code;
+      },
+    },],
 })
