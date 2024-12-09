2bdc78522a2644f913d30f367d3996e6fe87902e
saranin-programmer <aldan2550@gmail.com>
2024-12-09 15:54:44 +0700

    проба

--- src/Visual/AppVisual.tsx
+++ src/Visual/AppVisual.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router,  Route, Routes} from 'react-router-dom';
+import {HashRouter as Router,  Route, Routes} from 'react-router-dom';
 import HomePage from "./HomePage/HomePage";
 
 import Controller from "../Controller/Controller.tsx";
