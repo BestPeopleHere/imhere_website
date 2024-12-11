20c8e277e35b942ed02b6ec7b583a0b6ca5c25e1
”Anna <a.sibiryakova@g.nsu.ru>
2024-12-11 13:32:09 +0700

    fixed user profile, create user card

--- src/Visual/Search/UserCard/UserCard.tsx
+++ src/Visual/Search/UserCard/UserCard.tsx
@@ -3,51 +3,50 @@ import Button from "../../ItcHyFeeL/DoneElements/Button.tsx";
 import Element from "../../ItcHyFeeL/Element.tsx";
 import UserCardAvatar from "./CardElements/UserCardAvatar.tsx";
 import $ from "jquery";
-import GetInfProfileUser from "../../../Controller/GetInfProfileUser.tsx";
 import UserCardStatus from "./CardElements/UserCardStatus.tsx";
 import UserCardTag from "./CardElements/UserCardTag.tsx";
+import GetInfProfileUser from "../../../Controller/GetInfProfileUser.tsx";
 
 class UserCard extends Button {
+    private tags: { tag_name: string; id: number }[];
+
     constructor() {
         super();
+        this.tags = Array.from({ length: 5 }, (_, index) => ({
+            id: index,
+            tag_name: `Тег ${index + 1}`,
+        }));
     }
 
     render() {
+        console.log(this.tags); // проверяем, что данные корректны
         return (
             <div className="main-container-8">
                 <div className='rectangle-8'/>
                 <Element instance={this.userAvatar} className='avatar-user-card'/>
                 <Element instance={this.userStatus} className='status-user-card'/>
-                <span className='nickname-user-card'>anechka.s</span>
+                <span className='nickname-user-card'>Nickname</span>
                 <span className='status-user-card'>Статус</span>
 
-                <Element instance={this.userTag} className='tag'/>
-
-
-
-                <button className='rectangle-button'/>
-                <button className='rectangle-button-2'/>
-                <button className='rectangle-button-3'/>
-                <button className='rectangle-button-4'/>
-                <button className='rectangle-button-5'/>
-                <span className='tag'>Тег</span>
-                <span className='tag-6'>Тег</span>
-                <span className='tag-7'>Тег</span>
-                <span className='tag-8'>Тег</span>
-                <span className='tag-9'>Тег</span>
+                <div className="tags-container">
+                    {this.tags.map((tag) => (
+                        <Element
+                            key={tag.id}
+                            instance={new UserCardTag({label: tag.tag_name})}
+                            className="tag"
+                        />
+                    ))}
+                </div>
 
             </div>
-        )
-            ;
+        );
     }
 
     readyToBeRendered() {
         //Запускается при подготовке к работе.
-
     }
 
-    public setActionController(controller: GetInfProfileUser)
-    {
+    public setActionController(controller: GetInfProfileUser  ) {
         console.log(controller);
 
         this.controller = controller;
@@ -59,7 +58,6 @@ class UserCard extends Button {
 
     public userAvatar: UserCardAvatar = new UserCardAvatar();
     public userStatus: UserCardStatus = new UserCardStatus();
-    public userTag: UserCardTag = new UserCardTag();
 }
 
 export default UserCard;
